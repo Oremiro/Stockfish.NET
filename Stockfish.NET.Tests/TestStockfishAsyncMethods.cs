aa34614a8bb64ad4de63520573856eace3506c9c
Yaroslav Bondarev <dev.bondarev@gmail.com>
2020-11-13 01:16:21 +0300

    Add Test for TDD setup

---
+++ Stockfish.NET.Tests/TestStockfishAsyncMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Stockfish.NET.Tests
+{
+    public class TestStockfishAsyncMethods
+    {
+        [Fact]
+        async Task TestSetPositionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task<string> TestGetBoardVisualAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task<string> TestGetFenPositionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task<string> TestGetSkillLevelAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task TestSetFenPositionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task TestGetBestMoveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task TestGetBestMoveTimeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task<bool> TestIsMoveCorrectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task TestGetEvaluationAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        async Task TestSetDepthAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Stockfish.NET.Tests/TestStockfishMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Stockfish.NET.Tests
+{
+    public class TestStockfishMethods
+    {
+        [Fact]
+        void TestSetPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        string TestGetBoardVisual()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        string TestGetFenPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        string TestGetSkillLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void TestSetFenPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void TestGetBestMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void TestGetBestMoveTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        bool TestIsMoveCorrect()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void TestGetEvaluation()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void TestSetDepth()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
