90a6124963d9bad0f3f5e7497fa2410be5290a3d
root <root@DESKTOP-32L6PRK.localdomain>
2020-11-13 01:00:50 +0300

    Initial commit

---
+++ Stockfish.NET/IStockfishClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Stockfish.NET
+{
+    public interface IStockfishClient
+    {
+        # region Step by step methods
+        void SetPosition(List<string> moves = null);
+        string GetBoardVisual();
+        string GetFenPosition();
+        string GetSkillLevel(int skillLevel = 20);
+        void SetFenPosition(string fenPosition);
+        void GetBestMove();
+        void GetBestMoveTime(int time = 1000);
+        bool IsMoveCorrect(string moveValue);
+        void GetEvaluation();
+        void SetDepth();
+        # endregion
+        
+        # region Async methods
+        Task SetPositionAsync(List<string> moves = null);
+        Task<string> GetBoardVisualAsync();
+        Task<string> GetFenPositionAsync();
+        Task<string> GetSkillLevelAsync(int skillLevel = 20);
+        Task SetFenPositionAsync(string fenPosition);
+        Task GetBestMoveAsync();
+        Task GetBestMoveTimeAsync(int time = 1000);
+        Task<bool> IsMoveCorrectAsync(string moveValue);
+        Task GetEvaluationAsync();
+        Task SetDepthAsync();
+        # endregion
+    }
+}
---
+++ Stockfish.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stockfish.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
