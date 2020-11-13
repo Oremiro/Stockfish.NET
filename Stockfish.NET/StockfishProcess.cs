8e2f2e08affb8da5b7605d04207dece9c1b9668d
Yaroslav Bondarev <dev.bondarev@gmail.com>
2020-11-13 18:18:18 +0300

    Clean up, add models folder, add some docs to process file

---
+++ Stockfish.NET/StockfishProcess.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace Stockfish.NET
+{
+    public class StockfishProcess
+    {
+        /// <summary>
+        /// Default process info for Stockfish process
+        /// </summary>
+        private ProcessStartInfo _processStartInfo { get; set; }
+
+        /// <summary>
+        /// Stockfish process
+        /// </summary>
+        private Process _process { get; set; }
+
+        /// <summary>
+        /// Stockfish process constructor
+        /// </summary>
+        /// <param name="path">Path to usable binary file from stockfish site</param>
+        public StockfishProcess(string path)
+        {
+            _processStartInfo = new ProcessStartInfo
+            {
+                FileName =
+                    @"D:\Projects\Stockfish\Stockfish.NET\Stockfish.NET\Stockfish\win\stockfish_12_win_x64\stockfish_20090216_x64.exe",
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true
+            };
+            _process = new Process {StartInfo = _processStartInfo};
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="millisecond"></param>
+        public void Wait(int millisecond)
+        {
+            this._process.WaitForExit(millisecond);
+        }
+
+        /// <summary>
+        /// This method is writing in stdin of Stockfish process
+        /// </summary>
+        /// <param name="command"></param>
+        public void WriteLine(string command)
+        {
+            _process.StandardInput.WriteLine(command);
+            _process.StandardInput.Flush();
+        }
+        
+        /// <summary>
+        /// This method is allowing to read stdout of Stockfish process
+        /// </summary>
+        /// <returns></returns>
+        public string ReadLine()
+        {
+            return _process.StandardOutput.ReadLine();
+        }
+        /// <summary>
+        /// Start stockfish process
+        /// </summary>
+        public void Start()
+        {
+            _process.Start();
+        }
+        /// <summary>
+        /// This method is allowing to close Stockfish process
+        /// </summary>
+        ~StockfishProcess()
+        {
+            //When process is going to be destructed => we are going to close stockfish process
+            _process.Close();
+        }
+    }
+}
