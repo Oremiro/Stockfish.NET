e4b2bef97f76fd6148e0bf1f645d1af01ac08336
Yaroslav Bondarev <dev.bondarev@gmail.com>
2020-11-14 18:16:39 +0300

    Add more tests

--- Stockfish.NET.Tests/TestStockfishMethods.cs
+++ Stockfish.NET.Tests/TestStockfishMethods.cs
@@ -25,5 +25,48 @@ namespace Stockfish.NET.Tests
                 "d2d4"
             });
         }
+
+        [Fact]
+        public void TestGetBestMoveTimeFirstMove()
+        {
+            var bestMove = Stockfish.GetBestMoveTime();
+            Assert.Contains(bestMove, new List<string>
+            {
+                "e2e3",
+                "e2e4",
+                "g1f3",
+                "b1c3",
+                "d2d4"
+            });
+        }
+
+        [Fact]
+        public void TestGetBestMoveNotFirstMove()
+        {
+            Stockfish.SetPosition(new List<string>
+            {
+                "e2e4", "e7e6" 
+            });
+            var bestMove = Stockfish.GetBestMove();
+            Assert.Contains(bestMove, new List<string>
+            {
+                "d2d4", "g1f3"
+            });
+        }
+        
+        
+        [Fact]
+        public void TestGetBestMoveTimeNotFirstMove()
+        {
+            Stockfish.SetPosition(new List<string>
+            {
+                "e2e4", "e7e6" 
+            });
+            var bestMove = Stockfish.GetBestMoveTime();
+            Assert.Contains(bestMove, new List<string>
+            {
+                "d2d4", "g1f3"
+            });
+        }
     }
 }
